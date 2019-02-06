e6a39b03aaaef2d80c395c792732b2ef00071c57
Andras Molnar <andris9016@gmail.com>
2019-02-06 12:32:45 +0100

    Add data logic from connection to data_manager

---
+++ data_manager.py
@@ -0,0 +1,18 @@
+import connection
+from datetime import datetime
+
+
+def sort_file():
+    questions = connection.read_file('sample_data/question.csv')
+    questions.sort(key=lambda line: line['submission_time'], reverse=True)
+    return questions
+
+
+def formatted_file():
+    sorted_questions = sort_file()
+    datas = []
+    for question in sorted_questions:
+        ts = int(question['submission_time'])
+        question['submission_time'] = datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+        datas.append(question)
+    return datas
--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, redirect, url_for, request
-import connection
+import data_manager
 
 app = Flask(__name__)
 
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/list')
 def route_list():
-    stored_questions = connection.formatted_file()
+    stored_questions = data_manager.formatted_file()
     return render_template('list.html', questions=stored_questions)
 
 
