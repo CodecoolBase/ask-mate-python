9fb69387614c047aeba594b079d0c35bf775eb61
Andras Molnar <andris9016@gmail.com>
2019-02-06 11:15:03 +0100

    Implement server to run

---
+++ server.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, redirect, url_for, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/list')
+def route_list():
+    return render_template('list.html')
+
+
+if __name__ == "__main__":
+    app.run(
+        debug=True,
+        port=5000
+    )
