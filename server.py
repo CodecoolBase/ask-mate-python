00683489f286374a8c63d720ce380f72f5bc6f70
Andras Molnar <andris9016@gmail.com>
2019-02-06 12:27:10 +0100

    Change route_list func

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, redirect, url_for, request
+import connection
 
 app = Flask(__name__)
 
@@ -6,7 +7,8 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/list')
 def route_list():
-    return render_template('list.html')
+    stored_questions = connection.formatted_file()
+    return render_template('list.html', questions=stored_questions)
 
 
 if __name__ == "__main__":
